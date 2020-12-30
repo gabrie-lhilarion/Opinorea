a243ecbcbaf3007ea0038da013bb9ea080083422
gabrielHilarion <gabrielhhilarion@gmail.com>
2020-12-30 09:50:25 +0100

    update myscript.js, update apllication.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 import "../scripts/myscript.js"
 import "../styles/application.scss"
-import "~bootstrap-icons/font/bootstrap-icons.css"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- app/javascript/scripts/myscript.js
+++ app/javascript/scripts/myscript.js
@@ -1,1 +1,32 @@
- 
+(() => {
+    
+      const action_button = document.querySelector('.action-icons'); 
+      const mobile_icon_slide_in = document.getElementById('mobile-icon-slide-in');
+
+      let toggleMenu = () => { 
+        const menuStyle = {};
+        screen.width > 600 ? menuStyle.width  = '350px' : menuStyle.width = screen.width 
+        menuStyle.height = (screen.height - 160) + 'px'
+        mobile_icon_slide_in.classList.toggle('show')
+        mobile_icon_slide_in.className == 'show' ? menuStyle.display = 'block' : menuStyle.display = 'none'
+        Object.assign(document.getElementById('mobile-icon-slide-in').style, menuStyle); 
+      }
+     
+      let toggle_SlidIn_Menu = (x) => {
+            if(x.target.id == 'fa-menu') {
+                toggleMenu();
+            }
+      }
+
+      action_button.addEventListener( 'click', toggle_SlidIn_Menu );
+
+      let hide_Slidein_Menu = () => {
+        if( window.innerWidth >= 600 ){
+             document.getElementById('mobile-icon-slide-in').style.display = 'none' 
+             mobile_icon_slide_in.classList.toggle('show')
+            }
+      }
+
+      window.addEventListener('resize', hide_Slidein_Menu);
+
+ })();
