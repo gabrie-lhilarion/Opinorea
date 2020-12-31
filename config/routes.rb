a47c528ee18590aaf30896f308f77c8d92caf0a8
gabrielHilarion <gabrielhhilarion@gmail.com>
2020-12-31 12:22:03 +0100

    update myscript.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :update]
+  resources :users, only: [:show, :index, :update]
 
   root to: "users#show"
-
 end
