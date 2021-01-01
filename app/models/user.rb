d023981ecf4da7221a5128a02346bc798b6b6af4
gabrielHilarion <gabrielhhilarion@gmail.com>
2021-01-01 10:27:34 +0100

    create and migrate opinion and folloeing models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
   
+  has_many :opinions
+  has_many :followings
+
+  validates_presence_of :fullname, :photo, :cover_image
+  validates_uniqueness_of :email, :username, case_sensitive: false 
+  validates_associated :opinions, :followings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_115837) do
+ActiveRecord::Schema.define(version: 2021_01_01_080223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "follower"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_followings_on_user_id"
+  end
+
+  create_table "opinions", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_opinions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +47,6 @@ ActiveRecord::Schema.define(version: 2020_12_24_115837) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "followings", "users"
+  add_foreign_key "opinions", "users"
 end
