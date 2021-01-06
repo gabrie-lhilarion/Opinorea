5c15d8b836d97412c67a8289fb0660d094571976
gabrielHilarion <gabrielhhilarion@gmail.com>
2021-01-06 10:33:59 +0100

    installed and migrated active storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :authenticate_user!
     layout 'sessions', if: :devise_controller?
+
     
     protected
   
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :photo, :cover_image])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :username, :email, :photo, :cover_image])
     end
 end
