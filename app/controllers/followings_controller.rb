5d37d030f792127fdc1c4d3c45a914d324e3596d
gabrielHilarion <gabrielhhilarion@gmail.com>
2021-03-10 21:44:02 +0100

    update followings _controller.rb

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,23 +1,22 @@
 class FollowingsController < ApplicationController
   def create
     followed = User.find(params[:id])
-
-    if Following.not_follower?(followed.id, current_user.id)
-      followed.followings.create(follower_id: current_user.id, follower_name: current_user.fullname)
-      redirect_to root_path, notice: "You have followed @#{followed.username} successfully."
+    
+    if Following.following(current_user).include?(followed)
+      redirect_to root_path, notice: "You are already following @#{followed.username}."
     else
-      redirect_to root_path, notice: "You could not follow @#{followed.username}."
+      followed.followings.create(follower_id: current_user.id, follower_name: current_user.fullname)
+      redirect_to user_path(followed), notice: "You have followed @#{followed.username} successfully."
     end
   end
 
   def destroy
-    @follower = Following.find(params[:id])
-    id = @follower.user_id
-    followed = User.find(id).fullname
+    @follower = Following.find_by(id: params[:id]) 
+     
     if @follower.destroy
-      redirect_to root_path, notice: "You have unfollowed #{followed} successfully."
+      redirect_to root_path, notice: "Unfollow was successfully."
     else
-      redirect_to root_path, notice: "Could not unfollow @#{followed}!!"
+      redirect_to root_path, notice: "Unfollow was not successfull!"
     end
   end
 end
