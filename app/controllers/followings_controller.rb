b8e9fd504812d62f2d501ff546cfec35fc6fe677
gabrielHilarion <gabrielhhilarion@gmail.com>
2021-03-16 05:46:15 +0100

    create display_image and display_cover,  helpers that will display image if attached or display innitials

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,20 +1,27 @@
 class FollowingsController < ApplicationController
   def create
     followed = User.find(params[:id])
-    if Following.following(current_user).include?(followed)
+    following = followed.followings.build(follower_id: current_user.id, follower_name: current_user.fullname)
+
+    if Following.a_follower?(followed.id, current_user.id)
       redirect_to root_path, notice: "You are already following @#{followed.username}."
     else
-      followed.followings.create(follower_id: current_user.id, follower_name: current_user.fullname)
-      redirect_to user_path(followed), notice: "You have followed @#{followed.username} successfully."
+      if following.save
+        redirect_to root_path, notice: "You have followed @#{followed.username} successfully."
+      else
+        redirect_to user_path(current_user), notice: "Could not follow @#{followed.username}."
+      end
     end
   end
 
   def destroy
-    @follower = Following.find_by(id: params[:id])
+    @follower = Following.find(params[:id]) 
+    id = @follower.user_id
+    followed = User.find(id).fullname
     if @follower.destroy
-      redirect_to root_path, notice: 'Unfollow was successfully.'
+      redirect_to root_path, notice: "You have unfollowed #{followed} successfully."
     else
-      redirect_to root_path, notice: 'Unfollow was not successfull!'
+      redirect_to root_path, notice: "Could not unfollow @#{followed}!!"
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+    
+  def display_image(user_id,photo)
+    user = User.find(user_id) 
+    if user.photo.attached?
+     image_tag photo, style: "width: inherit; height: inherit"
+    else  
+     ''
+    end
+  end
+
+  def display_cover(user_id,cover_image)
+    user = User.find(user_id)
+    if user.cover_image.attached?
+     image_tag cover_image, style: "width: 100%; height: auto"
+    else  
+     "<h1 class=\"cover_heading\">#{user.fullname}</h1> <h4 class=\"cover_subheading\"> <em> @#{user.username} </em> </h4>".html_safe
+    end
+  end
+
 end
