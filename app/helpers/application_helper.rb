d931a466af50b2f24409b8bdf247155c5e9321cf
gabrielHilarion <gabrielhhilarion@gmail.com>
2021-04-01 06:47:13 +0100

    Update 'not_deleted' method to not_deleted?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def display_image(user_id, photo)
     user = User.find(user_id)
-    if user.photo.attached? && not_deleted(user.photo.attachment)
+    if user.photo.attached? && not_deleted?(user.photo.attachment)
       image_tag photo, style: 'width: 55px; max-height: 55px'
     else
       "<span class=\"inside-round\">#{user.fullname[0]}</span>".html_safe
@@ -10,7 +10,7 @@ module ApplicationHelper
 
   def display_cover(user_id, cover_image)
     user = User.find(user_id)
-    if user.cover_image.attached? && not_deleted(user.cover_image.attachment)
+    if user.cover_image.attached? && not_deleted?(user.cover_image.attachment)
       image_tag cover_image, style: 'width: 100%; height: auto'
     else
       full = user.fullname
@@ -19,7 +19,7 @@ module ApplicationHelper
     end
   end
 
-  def not_deleted(file)
+  def not_deleted?(file)
     if !file.nil?
       full_file_path = File.absolute_path(file.filename.to_s)
       File.exist?(full_file_path)
