f56ecca7f46894d040241bb5371144d04d5121fa
gabrie-lhilarion <gabrielhilarion@gmail.com>
2021-09-02 09:35:28 +0100

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,5 +24,5 @@ import "../styles/application.css"
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+//const images = require.context('../images', true)
+//const imagePath = (name) => images(name, true)
